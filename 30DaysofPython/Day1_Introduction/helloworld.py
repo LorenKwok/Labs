2ec57e4a278da64a1378b19e379ac6bb2414a2a3
LorenKwok <lorenlykwok@email.com>
2026-06-20 21:03:15 -0700

    Day1

---
+++ 30DaysofPython/Day1_Introduction/helloworld.py
@@ -0,0 +1,38 @@
+#30 Days of Python# 
+#Day 1 - Introduction#
+
+print(3+4) #addition(+)
+print(4-3) #subtraction(-)
+print(4*3) #multiplication(*)
+print(4/3) #division(/)
+print(4**3) #exponential(**)
+print(4%3) #modulus(%)
+print(4//3) #floor division operator(//)
+
+#Write strings on the python interactive shell. The strings are the following:
+print("Loren")
+print("Kwok")
+print("Canada")
+print("I am enjoying 30 days of python")
+
+print(type(10)) #integer
+print(type(9.8)) #float
+print(type(3.14)) #float
+print(type(4-4j)) #complex
+print(type(['Asabeneh','Python','Finland'])) #list
+print(type("Loren")) #string
+print(type("Kwok")) #string
+print(type("Canada")) #string
+
+#Examples of Python data types
+print(type(7)) #integer
+print(type(4.82)) #float
+print(type(28+4j)) #complex
+print(type("Hello")) #string
+print(type(True)) #boolean
+print(type([2,3,5,7])) #list
+print(type(("Tony","Steve","Bruce","Thor","Clint"))) #tuple
+print(type({"Tony","Steve","Bruce","Thor","Clint"})) #set
+
+#Find an Euclidean distance between (2,3) and (10,8)
+print("d","=",((10-2)**2+(8-3)**2)**0.5)                                       
